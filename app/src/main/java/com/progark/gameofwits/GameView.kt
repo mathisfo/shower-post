6dc09c75f19894df7b8b2caa8774136d5856dc19
kristoffer <kristnyv@stud.ntnu.no>
2022-04-07 14:14:13 +0200

    add starter skeleton code

--- app/src/main/java/com/progark/gameofwits/GameView.kt
+++ app/src/main/java/com/progark/gameofwits/GameView.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class GameView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.gameview)
+
+        fetchLetters()
         val btn: Button = findViewById(R.id.backButton)
         val endofgame: Button = findViewById(R.id.endofgamebtn)
 
@@ -16,6 +20,14 @@ class GameView : AppCompatActivity() {
         endofgame.setOnClickListener { openEndOfGameView() }
     }
 
+    private fun fetchLetters() {
+        val db = Firebase.firestore
+
+        val letters = db.collection("letters").document("letters").get().addOnSuccessListener { letterArray ->
+            println(letterArray.data)
+        }
+
+    }
     private fun openLobbyView() {
         val intent = Intent(this, LobbyView::class.java)
         startActivity(intent)
